542d09ad09aa28c1da6b52b4738ac7540b3c000a
Mack Maier <hmaiermack@gmail.com>
2021-06-03 12:00:01 -0700

    void type

--- dist/main.js
+++ dist/main.js
@@ -1,16 +1,6 @@
-//can use type aliases similar to property types
-var popularTags = ['dragon', 'coffe'];
-//can use unions when defining types, keeping you from repeating type definitions all over the place
-//eg const dragonsTag: PopularTag | null 
-var dragonsTag = "dragon";
-var username = 'mack';
-//unions -> | allow you to combine data types
-//most popular union is checking for null: eg for async fetching
-var pageNumber = '1';
-//when using a union whatever you set your variable to is the default value, if there is no default value it will default to undefined
-//let error: string | null would be type undefined, whereas the below example is type null
-var errorMessage = null;
-//you can also pass interfaces into unions
-var user = null;
-//unions can get out of control if you abuse them
-var someProp;
+//if function doesnt return something, its type defaults to void
+//it's still recomended to define it yourself for stricter type checking
+//eg if you try to return something you'll get an error rather than the type automatically changing
+var doSomething = function () {
+    console.log("doSomething");
+};
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,6 @@
-//similar to interfaces, types start with a capital letter
-type ID = string
-type PopularTag = string;
-//custom types + unions
-type MaybePopularTag = PopularTag | null
-//you can use type aliases inside of interfaces, giving you more info as to what the property is actually for
-interface UserInterface {
-    id: ID,
-    name: string,
-    lastName: string
+//if function doesnt return something, its type defaults to void
+//it's still recomended to define it yourself for stricter type checking
+//eg if you try to return something you'll get an error rather than the type automatically changing
+const doSomething = (): void => {
+    console.log("doSomething")
 }
-
-//can use type aliases similar to property types
-const popularTags: PopularTag[] = ['dragon', 'coffe']
-
-//can use unions when defining types, keeping you from repeating type definitions all over the place
-//eg const dragonsTag: PopularTag | null 
-const dragonsTag: MaybePopularTag = "dragon"
-
-let username: string = 'mack'
-//unions -> | allow you to combine data types
-//most popular union is checking for null: eg for async fetching
-let pageNumber: string | number = '1';
-
-//when using a union whatever you set your variable to is the default value, if there is no default value it will default to undefined
-//let error: string | null would be type undefined, whereas the below example is type null
-let errorMessage: string | null = null
-
-//you can also pass interfaces into unions
-let user: UserInterface | null = null
-
-//unions can get out of control if you abuse them
-let someProp: string | number | null | undefined | object | string[]
