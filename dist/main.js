0550ef05509e8f015357c9f044548276b59f5b5a
Mack Maier <hmaiermack@gmail.com>
2021-06-03 11:18:01 -0700

    interface functions

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,14 @@
 var user = {
     name: 'Mack',
-    age: 26
+    age: 26,
+    getMessage: function () {
+        return 'Hello' + name;
+    }
 };
 var user2 = {
-    name: "Jack"
+    name: "Jack",
+    getMessage: function () {
+        return 'Hello' + name;
+    }
 };
+console.log(user.getMessage());
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 //interface label should start with a capital letter
 //by default all properties in interface are mandatory
 //to make a property optional add a ? after the property name
+//interfaces provide autocomplete 
 interface User {
     name: string,
-    age?: number
+    age?: number,
+    getMessage(): string
 }
 
 const user: User = {
     name: 'Mack',
-    age: 26
+    age: 26,
+    getMessage() {
+        return 'Hello' + name
+    }
 }
 
 const user2: User = {
-    name: "Jack"
+    name: "Jack",
+    getMessage() {
+        return 'Hello' + name
+    }
 }
+
+console.log(user.getMessage())
