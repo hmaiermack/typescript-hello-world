69445fd8fbad5665e3a53fbf04fbea1010e7f671
Mack Maier <hmaiermack@gmail.com>
2021-06-03 17:04:54 -0700

    typescript classes - public/private/protected - readonly/static - implements interface - extends another class

--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,57 @@
-//typescript doesn't parse markup
-//when working with DOM elements be as specific as possible when defining your types
-//in the example below, if you do not assert that the element .foo is not an HTMLInputElement then you wouldnt be able to access it's value
-var someEl = document.querySelector(".foo");
-//element is the highest class in the dom hierarchy and very generic
-console.log('someElement', someEl.value);
-//similarly to element, event is a "super generic" type and will be automatically assigned
-someEl.addEventListener('blur', function (event) {
-    //create a new variable with a specific type assertion so that you can correctly access its value
-    //if you tried to access the value without defining target as an HTMLInputElement via event.target.value
-    //typescript will throw an error
-    var target = event.target;
-    console.log("event", target.value);
-});
-//when working with the DOM in typescript you have to use as to assign the correct types to whatever it is you're working with
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//if you define an interface and have a class implement it, then that interface's properties must be included in the class
+var User = /** @class */ (function () {
+    function User(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.unchangeableName = firstName;
+    }
+    //as unchangeableName is readonly you are only able to set the value once, afterwards it cannot be changed
+    User.prototype.changeUnchangeableName = function () {
+        this.unchangeableName = 'foo';
+    };
+    //for example, if we commented out this function the class would throw an error because it must include a function called getFullName
+    User.prototype.getFullName = function () {
+        return this.firstName + ' ' + this.lastName;
+    };
+    //static properties are something you get from the class itself and not passed down to its children
+    User.maxAge = 50;
+    return User;
+}());
+//this creates a new class Admin with all properties from class User that you can extend with new properties only accessible from Admin
+var Admin = /** @class */ (function (_super) {
+    __extends(Admin, _super);
+    function Admin() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Admin.prototype.setEditor = function (editor) {
+        this.editor = editor;
+    };
+    Admin.prototype.getEditor = function () {
+        return this.editor;
+    };
+    return Admin;
+}(User));
+var admin = new Admin('foo', 'bar');
+//by default, everything in a class is public meaning that it can be used anywhere
+//if you set a property to private, that means you can only use it inside of a class
+//if protected, the property is allowed in the class and its children
+var user = new User('mack', 'maier');
+//as an example of above, you can no longer access firstName and lastName from class User
+console.log(user.getFullName());
+console.log(admin.getFullName);
+//when typescript is transpiled, none of public/private/protected exists
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,57 @@
-//typescript doesn't parse markup
-//when working with DOM elements be as specific as possible when defining your types
-//in the example below, if you do not assert that the element .foo is not an HTMLInputElement then you wouldnt be able to access it's value
-const someEl = document.querySelector(".foo") as HTMLInputElement
+//classes are syntactic sugar for prototypes
+interface UserInterface {
+    getFullName(): string
+}
 
-//element is the highest class in the dom hierarchy and very generic
-console.log('someElement', someEl.value)
+//if you define an interface and have a class implement it, then that interface's properties must be included in the class
+class User implements UserInterface{
+    protected firstName: string
+    private lastName: string
+    readonly unchangeableName
+    //static properties are something you get from the class itself and not passed down to its children
+    static readonly maxAge = 50
 
-//similarly to element, event is a "super generic" type and will be automatically assigned
-someEl.addEventListener('blur', (event) => {
-    //create a new variable with a specific type assertion so that you can correctly access its value
-    //if you tried to access the value without defining target as an HTMLInputElement via event.target.value
-    //typescript will throw an error
-    const target = event.target as HTMLInputElement
-    console.log("event", target.value)
-})
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this. unchangeableName = firstName
+    }
 
-//when working with the DOM in typescript you have to use as to assign the correct types to whatever it is you're working with
+
+    //as unchangeableName is readonly you are only able to set the value once, afterwards it cannot be changed
+    changeUnchangeableName(): void {
+        this.unchangeableName = 'foo'
+    }
+
+    //for example, if we commented out this function the class would throw an error because it must include a function called getFullName
+    getFullName(): string {
+        return this.firstName + ' ' + this.lastName
+    }
+}
+
+//this creates a new class Admin with all properties from class User that you can extend with new properties only accessible from Admin
+class Admin extends User {
+    private editor: string
+    
+    setEditor(editor: string): void {
+        this.editor = editor
+    }
+
+    getEditor(): string {
+        return this.editor
+    }
+}
+
+const admin = new Admin('foo', 'bar')
+
+//by default, everything in a class is public meaning that it can be used anywhere
+//if you set a property to private, that means you can only use it inside of a class
+//if protected, the property is allowed in the class and its children
+const user = new User('mack', 'maier')
+
+//as an example of above, you can no longer access firstName and lastName from class User
+console.log(user.getFullName())
+
+console.log(admin.getFullName)
+
+//when typescript is transpiled, none of public/private/protected exists
